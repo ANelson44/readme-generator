4697f7c3e9713bddc959961887e2d56cd627a0be
ANelson44 <145131926+ANelson44@users.noreply.github.com>
2023-12-05 15:55:46 -0500

    added  questions to array and function to init app

--- Develop/index.js
+++ Develop/index.js
@@ -15,6 +15,48 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Provide a short description of the purpose and functionality of the project.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose an applicable license',
+        choices: ('MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'none'),
+    },
+    {
+        type: 'input',
+        name: 'require',
+        message: 'List any required dependencies.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Which languages or technologies are used for this project?'
+    },
+    {
+        type: 'input',
+        name: 'creator',
+        message: 'Write your GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your first and last name?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your preferred email address?',
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'list any other contributors if applicable (use GitHub usernames)',
+        default: '',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'list details of any testing if applicable.'
     }
 ];
 
@@ -24,7 +66,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log('Creating Professional README.md File...');
+        writeToFile('./dist/README.md', generateMarkdown({...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
